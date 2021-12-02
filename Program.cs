a77654a6afedd2d5b8f7117ec8600b3cc77c25df
DEV\aklein <Andreas.Kleinbichler@admiral.at>
2021-12-02 20:18:27 +0100

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Example.Log4Net.logging;
+using System;
+using System.IO;
+
+namespace Example.Log4Net
+{
+    class Program
+    {
+        private static ILoggerWrapper logger = LoggerFactory.GetLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Debug("This is a debug message.");
+            logger.Fatal("This is a fatal message.");
+            logger.Warn("This is a warning message.");
+            logger.Error("This is an error message.");
+        }
+    }
+}
